5cd83e58dbe99db987f80845e96362137a3717b5
margri764 <62308899+margri764@users.noreply.github.com>
2022-03-06 20:30:10 -0300

    deploy

--- src/app/shared/pages/home/home.component.ts
+++ src/app/shared/pages/home/home.component.ts
@@ -8,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-
+public imgEcurow : string='';
 
   constructor() { }
 
   ngOnInit(): void {
+
+    (screen.width<600) ?  this.imgEcurow="./assets/ecuRow_cel.png" :     this.imgEcurow="./assets/ecuRow-desktop.png";
+    // if(screen.width > 961 && screen.width <= 1280){
+    //  this.imgEcurow="./assets/ecuRow-desktop.png";
+
+    //  (screen.width > 300 && screen.width < 700) ?
+    //   [this.bufferX = 100, this.bufferY = 100]         
+    //   :[ this.bufferX = 15, this.bufferY = 15]
+    
+      
+
 
   }
 
