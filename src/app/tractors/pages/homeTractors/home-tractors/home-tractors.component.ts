123aa0e7a3c75d63b678ef036d1f005521bd65a3
margri764 <62308899+margri764@users.noreply.github.com>
2022-11-24 10:35:58 -0300

    ready to VPS

--- src/app/tractors/pages/homeTractors/home-tractors/home-tractors.component.ts
+++ src/app/tractors/pages/homeTractors/home-tractors/home-tractors.component.ts
@@ -13,11 +13,11 @@ export class HomeTractorsComponent implements OnInit, OnInit{
   @HostListener('window:scroll') onScroll(e: Event): void {
     console.log(this.getYPosition(e));
     (this.getYPosition(e) >= 3 ) ? this.scroll = true : this.scroll = false;
-    (this.getYPosition(e) >= 30 ) ? this.scroll2 = true : this.scroll2 = false;
-    (this.getYPosition(e) >= 47 ) ? this.scroll3 = true : this.scroll3 = false;
-    (this.getYPosition(e) >= 67 ) ? this.scroll4 = true : this.scroll4 = false;
-    (this.getYPosition(e) >= 80 ) ? this.scroll5 = true : this.scroll5 = false;
-    (this.getYPosition(e) >= 93 ) ? this.scroll6 = true : this.scroll6 = false;
+    (this.getYPosition(e) >= 25 ) ? this.scroll2 = true : this.scroll2 = false;
+    (this.getYPosition(e) >= 40 ) ? this.scroll3 = true : this.scroll3 = false;
+    (this.getYPosition(e) >= 55 ) ? this.scroll4 = true : this.scroll4 = false;
+    (this.getYPosition(e) >= 68 ) ? this.scroll5 = true : this.scroll5 = false;
+    (this.getYPosition(e) >= 75 ) ? this.scroll6 = true : this.scroll6 = false;
     
   }
 
